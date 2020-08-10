fa300680e610c7976c3b1127207e2c6b826fc65b
Natania Safa Ramadhani <polomeng20@gmail.com>
2020-08-10 22:45:18 +0700

    tolong regis loginnya kwan

---
+++ app/src/main/java/com/shinta/idn/chitchat/WelcomeActivity.kt
@@ -0,0 +1,42 @@
+package com.shinta.idn.chitchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.shinta.idn.chitchat.R
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    var firebaseUser: FirebaseUser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        btn_welcome_sign_up.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        btn_welcome_signin.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        if (firebaseUser != null){
+            val intent  = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/shinta/idn/chitchat/model/ChatList.kt
@@ -0,0 +1,17 @@
+package com.shinta.idn.chitchat.model
+
+class ChatList {
+    private var id: String = ""
+    constructor()
+    constructor(id: String) {
+        this.id = id
+    }
+
+    fun getId(): String? {
+        return id
+    }
+
+    fun setId (id: String?){
+        this.id = id!!
+    }
+}
